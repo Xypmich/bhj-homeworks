b6f1c4013e85e3188af063c506e635d848fc40a8
Xypmich <kvudev@gmail.com>
2023-02-13 19:33:41 +0300

    fix remarks

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -4,9 +4,13 @@ menus.forEach(menu => {
     let subMenus = menu.querySelectorAll('.menu_sub');
     subMenus.forEach(subMenu => {
         let parent = subMenu.closest('.menu__item');
-        parent.onclick = () => {
-            subMenus.forEach(subMenu => subMenu.className = 'menu menu_sub');
-            subMenu.className = 'menu menu_sub menu_active';
+        parent.querySelector('.menu__link').onclick = () => {
+            if (subMenu.className == 'menu menu_sub menu_active') {
+                subMenu.className = 'menu menu_sub';
+            } else {
+                subMenus.forEach(subMenu => subMenu.className = 'menu menu_sub');
+                subMenu.className = 'menu menu_sub menu_active';  
+            }
             return false
         }
     })
